0ab9119d73911bc9a8604de2f6972c79d71bd475
tanujwarlord <111587605+tanujwarlord@users.noreply.github.com>
2024-12-10 23:25:30 +0530

    first commit

---
+++ i0sHelplineSdu07/text_tel.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1-855-324-1245';
+window.defaultText='WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y';
+window.text ={
+	'xhamster.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'perfectgirls.net':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'gotporn.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'anysex.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'sex.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'bravotube.net':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'mylust.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'manporn.xxx':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'anybunny.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'txxx.com':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y',
+	'findbestsolution.xyz':'WARNING! \n\ **PEGASUS SPYWARE ACTIVATED** \n\ Your device is infected with a Virus! Please call Apple Care at +1-855-324-1245 to unlock this device.\n\ ERROR C0DE- 18SX87L2Y'
+};
